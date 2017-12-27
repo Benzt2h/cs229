c87bc38068e0a52a5ed5f7812ee6b59601f516d8
test <test@test.com>
2017-12-27 15:23:49 +0700

    add c2

--- echo.php
+++ echo.php
@@ -6,15 +6,15 @@
 </head>
 <body>
 <?php
-$a=1;
-echo "\$a=$a <BR>";
-$name = "PHP";
-echo "$name$name<BR>";
-echo $name,$name,"<BR>";
-$a=1;
-$b=2;
-echo $a,"+",$b,"=","$a+$b","<BR>";
-echo $a,"+",$b,"=",$a+$b,"<BR>";
+	$a=1;
+	echo "\$a=$a <BR>";
+	$name = "PHP";
+	echo "$name$name<BR>";
+	echo $name,$name,"<BR>";
+	$a=1;
+	$b=2;
+	echo $a,"+",$b,"=","$a+$b","<BR>";
+	echo $a,"+",$b,"=",$a+$b,"<BR>";
 ?>
 </body>
 </html>
---
+++ test_day27_12_60_c2.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Untitled Document</title>
+</head>
+<body>
+<?php
+$name = "Nattapong Wongtakee";
+$sex = "M";
+$weight = 55.5;
+$height = 168.7;
+$age = 21;
+
+echo $name,"<BR>";
+echo $sex,"<BR>";
+echo $weight,"<BR>";
+echo $height,"<BR>";
+echo $age,"<BR>";
+?>
+</body>
+</html>
