e9eee8a6122942c47bd17978e8835b31a2621c54
test <test@test.com>
2018-01-03 14:17:05 +0700

    add switch-case

---
+++ switch-case.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Untitled Document</title>
+</head>
+<body>
+<?php
+$english_Day=date("l");
+switch($english_Day)
+{
+	case "Monday";
+			$th_day = "จันทร์";
+			break;
+	case "Tuesday";
+			$th_day = "อังคาร";
+			break;
+	case "Wednesday";
+			$th_day = "พธุ";
+			break;
+	case "Thursday";
+			$th_day = "พฤหัส";
+			break;
+	case "Friday";
+			$th_day = "ศุกร์";
+			break;
+	case "Saturday";
+			$th_day = "เสาร์";
+			break;
+	
+	}
+?>
+</body>
+</html>
