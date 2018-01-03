b4e5f79cb126627b76bc5d82744876d8c7695afc
test <test@test.com>
2018-01-03 14:41:16 +0700

    add for

---
+++ for_test8-9.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Untitled Document</title>
+</head>
+<body>
+<?php
+/*for($count=1;$count<10;$count++){
+	echo "Hello <br>";
+	}
+	*/
+	
+$sum=0;
+for($count=1;$count<=10;$count++){
+	$sum+=$count;
+	}
+	echo "ผลบวกของเลข 1-10 คือ $sum";
+?>
+</body>
+</html>
