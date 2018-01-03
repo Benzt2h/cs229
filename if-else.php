d56f37b2210b9eb26e31510504b7a5ba17938eb3
test <test@test.com>
2018-01-03 13:31:51 +0700

    fix name to if and add if-else

---
+++ if-else.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Untitled Document</title>
+</head>
+<body>
+<?php
+$a=5;
+if($a!=5){
+	echo "a=$a<BR>";
+}
+else{
+	echo"a is zero. <BR>";
+	}
+?>
+</body>
+</html>
