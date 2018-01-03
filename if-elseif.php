599fde3a3a93b72f3721a6047ac4301d6d13d974
test <test@test.com>
2018-01-03 13:44:15 +0700

    add if-elseif

---
+++ if-elseif.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Untitled Document</title>
+</head>
+<body>
+<?php
+//$name = "Benz";
+$name = "leon";
+if($name == ""){
+	printf("Yon have a no name");
+	}
+else if($name=="leon" OR $name == "Leon"){
+	printf("Hello,Leon!");
+}
+else{
+	printf("Your name is '$name'");
+	}
+?>
+</body>
+</html>
