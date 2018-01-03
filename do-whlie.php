aea84f2f92b57758f8561577946d0d1c836bfaf4
test <test@test.com>
2018-01-03 14:31:38 +0700

    rename and add do-while

---
+++ do-whlie.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Untitled Document</title>
+</head>
+<body>
+<?php
+$count =1;
+do
+{
+	echo "Hello <br>";
+	$count++;
+}while($count<=10);
+?>
+</body>
+</html>
