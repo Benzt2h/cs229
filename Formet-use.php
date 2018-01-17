3adbedb0d2da87eb4a52da2bc1af6458fd8bce2d
test <test@test.com>
2018-01-17 14:55:51 +0700

    add Formet.php

---
+++ Formet-use.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Untitled Document</title>
+</head>
+<body>
+<?php
+echo "".max(2,5,9,1,5,3,4);
+echo "<BR>".log(5);
+echo "<BR>".Asin(0.5);
+echo "<BR>".deg2rad(60);
+echo "<BR>".DecBin(256);
+?>
+</body>
+</html>
