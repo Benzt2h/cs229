e65cdb02e2814af15cb0a02136a15c128abe31e8
test <test@test.com>
2018-01-17 14:15:14 +0700

    edit

--- function-var-global.php
+++ function-var-global.php
@@ -4,17 +4,20 @@
 <meta charset="utf-8">
 <title>Untitled Document</title>
 </head>
-<body>
 <?php
 function test(){
-	$a = 20;
+	global $a;
+	$a=20;
 	echo "ตัวแปร \$a นึ้อยู่ภายในฟังก์ชัน ทีค่าเท่ากับ $a <BR>";
 	}
 $a=200;
+test();
+$a+=2;
 echo "ตัวแปร \$a นึ้อยู่ภายนอกฟังก์ชัน มีค่าเท่ากับ $a <BR>";
 test();
 $a++;
 echo "ตัวแปร \$a นึ้อยู่ภายนอกฟังก์ชัน มีค่าเท่ากับ $a <BR>";	
 ?>
+<body>
 </body>
 </html>
