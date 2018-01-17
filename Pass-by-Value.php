1a47ee1357ed7c78027d531a45fbb22a79a73a95
test <test@test.com>
2018-01-17 14:26:26 +0700

    add Pass by Var and Ref

---
+++ Pass-by-Value.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Untitled Document</title>
+</head>
+<body>
+<?php
+function pass($val){
+	$val+=10;
+	return $val;
+}
+
+$value =100;
+echo "ตัวแปรมีค่าเท่ากับ $value<BR>";
+echo "ตัวแปรก่อนที่จะส่งผ่านค่าไปยังฟังก์ชัน มีค่าเท่ากับ $value <BR>";
+
+pass($value);
+echo "ตัวแปรหลังจากการคืนค่ากลับจากฟังก์ชัน มีค่าเท่ากับ $value";
+?>
+</body>
+</html>
